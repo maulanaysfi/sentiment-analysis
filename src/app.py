83924b7e299988cd54e9518fd470c8359c039ab7
Ibrahim <maulanayusuf5579@gmail.com>
2025-08-20 16:17:37 +0700

    Add HTML response (on "/")

--- src/app.py
+++ src/app.py
@@ -1,15 +1,29 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
-import joblib
+import joblib, os
 
 app = FastAPI()
+
+static_dir = os.path.join(os.path.dirname(__file__), "static")
+if not os.path.isdir(static_dir):
+    os.makedirs(static_dir)
+app.mount("/static", StaticFiles(directory=static_dir), name="static")
+
 model = joblib.load('models/model.joblib')
 
 class Query(BaseModel):
     text: str
 
+@app.get('/')
+def index():
+    with open(f"{static_dir}/index.html", "r", encoding="utf-8") as f:
+        html_content = f.read()
+    return HTMLResponse(content=html_content, status_code=200)
+
 @app.post("/predict")
 def predict(q: Query):
     pred = int(model.predict([q.text])[0])
-    label = ['negative', 'neutral', 'positive']
+    label = ['negative', 'neutral', 'positive'][pred]
     return {"label":label, "class_id":pred}
